966d7f03e549332ce0a97bd2ed4f071a3a0ea94a
hahnbi <hahnbi.sun@gmail.com>
2016-01-23 16:30:06 -0800

    Adjusted route skeleton, server typos

--- server/routes.js
+++ server/routes.js
@@ -3,10 +3,11 @@ var usersController = require('../users/userController.js');
 var solutionsController = require('../solutions/solutionController.js');
 
 module.exports = function (app) {
-  app.get('/api/challenges', function(req, res) {});
-  app.post('/api/challenges', function(req, res) {});
-  app.get('/api/users', function(req, res) {});
-  app.post('/api/users', function(req, res) {});
-  app.get('/api/solutions/:problemId', function(req, res) {});
-  app.post('/api/solutions/:problemId', function(req, res) {});
+  app.get('/api/challenges', function (req, res) {});
+  app.get('/api/challenges/:challengeId', function (req, res) {});
+  app.post('/api/challenges', function (req, res) {});
+  app.get('/api/users/:userId', function (req, res) {});
+  app.post('/api/users', function (req, res) {});
+  app.get('/api/solutions/:problemId', function (req, res) {});
+  app.post('/api/solutions/:problemId', function (req, res) {});
 };
--- server/server.js
+++ server/server.js
@@ -5,10 +5,9 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(bodyParser());
-app.use(express.static('../public'));
+app.use(express.static(__dirname + '/public'));
 
 require('routes.js')(app, express);
-
 
 app.listen(port);
 console.log('Server now listening on port ' + port);
