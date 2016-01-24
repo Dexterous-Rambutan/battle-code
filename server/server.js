369900d46a45937b3a686c7e7c0e40a81075a3be
Alan Fu <alanzfu@gmail.com>
2016-01-23 17:35:02 -0800

    Fixes directory routing in server and routes.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
 
-require('routes.js')(app, express);
+require('./routes.js')(app, express);
 
 app.listen(port);
 console.log('Server now listening on port ' + port);
