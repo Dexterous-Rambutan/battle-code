1a85e6e35c29decbc6ac33a9c5cf1862bec0e51d
kweng2 <kweng2@gmail.com>
2016-01-25 19:32:44 -0800

    user controller routes complete, including oauth routes

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,5 @@
 var challengesController = require('./challenges/challengeController.js');
-var usersController = require('./users/userController.js');
+var userController = require('./users/userController.js');
 var solutionsController = require('./solutions/solutionController.js');
 var passport = require('./helpers/psConfig.js');
 
@@ -13,24 +13,40 @@ module.exports = function (app) {
     passport.authenticate('github', { failureRedirect: '/login' }),
     function(req, res) {
       req.session.loggedIn = true;
+      console.log('Successfully logged in, ', req.session.passport.user);
+
+      // Add user to DB
+      req.body.github_handle = req.session.passport.user.username;
+      req.body.github_display_name = req.session.passport.user.displayName;
+      req.body.github_avatar_url = req.session.passport.user._json.avatar_url;
+      req.body.github_profileUrl = req.session.passport.user.profileUrl;
+      req.body.email = req.session.passport.user.emails[0].value;
+      userController.addUser(req);
+
+      // Once complete, redirect to home page
       res.redirect('/');
   });
 
   // When client needs to verify if they are authenticated
   app.get('/auth-verify', function(req, res) {
-    console.log('GET REQ to /auth-verify', req.session.loggedIn);
+    console.log('GET request to /auth-verify', req.session.loggedIn);
     if (req.session.loggedIn) {
-      res.end(JSON.stringify({auth: true}));
+      req.params.userId = req.session.passport.user.username || '';
+      userController.getUserById(req, res);
     } else {
-      res.end(JSON.stringify({auth: false}));
+      res.status(403).json({});
     }
   });
 
   app.get('/api/challenges', function (req, res) {});
   app.get('/api/challenges/:challengeId', function (req, res) {});
   app.post('/api/challenges', function (req, res) {});
-  app.get('/api/users/:userId', function (req, res) {});
-  app.post('/api/users', function (req, res) {});
+  app.get('/api/users/:userId', userController.getUserById);
+  app.post('/api/users', userController.addUser);
   app.get('/api/solutions/:challengeId', function (req, res) {});
   app.post('/api/solutions/:challengeId', function (req, res) {});
+  app.get('/logout', function (req, res) {
+    req.session.loggedIn = false;
+    res.redirect('/');
+  });
 };
--- server/users/usersCollection.js
+++
@@ -1,8 +0,0 @@
-var db = require('../dbConfig');
-var User = require('./userModel.js');
-
-var Users = db.Collection.extend({
-  model: User
-});
-
-module.exports = Users;
