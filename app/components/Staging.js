b3002f41ff9de05660b92007298f0ae36e6166b5
Alan Fu <alanzfu@gmail.com>
2016-01-28 11:57:31 -0800

    Spoof button and get request

--- app/components/Staging.js
+++ app/components/Staging.js
@@ -7,7 +7,7 @@ var Staging = React.createClass({
   render: function() {
     return (
       <div>
-        <button onClick={this.props.navActions.navSoloStaging}>SOLO</button>
+        <button onClick={this.props.navActions.navSoloStaging}>PRACTICE</button>
         <button onClick={this.props.navActions.navChallengeArena}>CHALLENGE</button>
       </div>
     )
