7e478f03de74b4a672b8b6d9a2cdbabb08df5004
kweng2 <kweng2@gmail.com>
2016-02-02 16:20:14 -0800

    Solo staging displays unique challenges, favoring completed ones over attmpted ones

--- app/components/SoloStaging.js
+++ app/components/SoloStaging.js
@@ -2,12 +2,27 @@ var React = require('react');
 
 var SoloStaging = React.createClass({
   render: function () {
-    var listOfProblems = this.props.user.user_problems.map(function (problem) {
+    // Only render unique challenges
+    var uniqueList = {};
+    var listOfProblems = [];
+    // Find every unique challenge, favoring completed over attempted
+    // In case a user attempts the problem twice, and only solves it once, only display the valid one
+    // In case a user attemps the problem once and does not solve it, display the attempted anyway
+    for (var i = 0; i < this.props.user.user_problems.length; i++ ) {
+      var problem = this.props.user.user_problems[i];
+      if (!(uniqueList[problem.challenge_id])) {
+        uniqueList[problem.challenge_id] = problem;
+      } else if (problem.valid) {
+        uniqueList[problem.challenge_id] = problem;
+      }
+    }
+    for (var key in uniqueList) {
+      var problem = uniqueList[key];
       var linkToProblem = function () {
         this.props.navActions.navSoloArena(problem);
       }.bind(this);
-      return <a href="#" onClick={linkToProblem}><li>Challenge ID:{problem.challenge_id} - {problem.valid ? 'Completed:' : 'Attempted:'} - {problem.end_time}</li></a>
-    }.bind(this));
+      listOfProblems.push(<a href="#" onClick={linkToProblem}><li>Challenge ID:{problem.challenge_id} - {problem.valid ? 'Completed:' : 'Attempted:'} - {problem.end_time}</li></a>);
+    }
 
     return (
       <div>
