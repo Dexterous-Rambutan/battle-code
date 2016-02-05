a059df930375875e55f81d16e89c5a27e3d1f1b3
kweng2 <kweng2@gmail.com>
2016-02-04 19:59:56 -0800

    added LEAVE button to the pairing view

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -6,13 +6,15 @@ var Nav = React.createClass({
   },
 
   render: function () {
+    var showNav = (this.props.user.isLoggedIn && (this.props.view !== 'CHALLENGE_ARENA' && this.props.view !== 'PAIR_ARENA'));
+    var showLeave = (this.props.user.isLoggedIn && (this.props.view === 'CHALLENGE_ARENA' || this.props.view === 'PAIR_ARENA'));
     return (
       <div className="nav-bar">
         <ul>
-          {this.props.user.isLoggedIn && this.props.view !== 'CHALLENGE_ARENA' ? <li><a href='/logout'>Logout</a></li> : null}
-          {this.props.user.isLoggedIn && this.props.view !== 'CHALLENGE_ARENA' ? <li><a href='#' onClick={this.navProfile}>Profile</a></li> : null}
-          {this.props.user.isLoggedIn && this.props.view !== 'CHALLENGE_ARENA' ? <li><a href='#' onClick={this.props.navActions.navStaging}>Modes</a></li> : null}
-          {this.props.user.isLoggedIn && this.props.view === 'CHALLENGE_ARENA' ? <li><a href='#' onClick={this.props.navActions.navAwayFromArena}>LEAVE</a></li> : null}
+          {showNav ? <li><a href='/logout'>Logout</a></li> : null}
+          {showNav ? <li><a href='#' onClick={this.navProfile}>Profile</a></li> : null}
+          {showNav ? <li><a href='#' onClick={this.props.navActions.navStaging}>Modes</a></li> : null}
+          {showLeave ? <li><a href='#' onClick={this.props.navActions.navAwayFromArena}>LEAVE</a></li> : null}
         </ul>
       </div>
     );
