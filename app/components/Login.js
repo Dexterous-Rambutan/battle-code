c6d6a2af3b70af711629dfe2c62f607620d91ecf
hahnbi <hahnbi.sun@gmail.com>
2016-02-09 11:43:27 -0800

    Basic splash page

--- app/components/Login.js
+++ app/components/Login.js
@@ -3,8 +3,20 @@ var React = require('react');
 var Login = React.createClass({
   render: function() {
     return (
-      <div>
-        <a href="/auth/github">Login with GitHub</a>
+      <div className="splash">
+        <div className="splash-logo">
+          <img src="/img/logo_full.png" />
+        </div>
+        <a href="/auth/github">
+          <div className="github-login card card-clickable">
+            <div className="github-login-handle">
+              <img src="img/github_logo.png" />
+            </div>
+            <div className="card-content">
+            <h3>Login with GitHub</h3>
+            </div>
+          </div>
+        </a>
       </div>
     )
   }
