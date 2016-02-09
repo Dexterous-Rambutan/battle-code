de46ddb3269d229db5ad5173e58cec9ea7729c88
Harun Davood <davoodharun@gmail.com>
2016-02-08 18:22:09 -0800

    more fixes

--- server/users/userController.js
+++ server/users/userController.js
@@ -26,7 +26,7 @@ userController.addUser = function ( req, res ) {
     github_handle: req.body.github_handle,
     github_display_name: req.body.github_display_name,
     github_avatar_url: req.body.github_avatar_url,
-    github_profile_url: req.body.github_profileUrl,
+    github_profile_url: req.body.github_profile_url,
     elo_rating: 1000,
     email: req.body.email
   };
@@ -56,7 +56,7 @@ userController.resetWithData = function() {
     github_handle: 'alanzfu',
     github_display_name: 'Alan Fu',
     github_avatar_url: 'https://avatars0.githubusercontent.com/u/7851211?v=3&s=460',
-    github_profileUrl: 'https://github.com/alanzfu',
+    github_profile_url: 'https://github.com/alanzfu',
     elo_rating:2000,
     email: null
   }).save().then(function() {
@@ -64,7 +64,7 @@ userController.resetWithData = function() {
       github_handle: 'puzzlehe4d',
       github_display_name: 'Harun Davood',
       github_avatar_url: 'https://avatars2.githubusercontent.com/u/12518929?v=3&s=460',
-      github_profileUrl: 'https://github.com/puzzlehe4d',
+      github_profile_url: 'https://github.com/puzzlehe4d',
       elo_rating:2000,
       email: null
     }).save();
@@ -73,7 +73,7 @@ userController.resetWithData = function() {
       github_handle: 'kweng2',
       github_display_name: 'Kevin Weng',
       github_avatar_url: 'https://avatars2.githubusercontent.com/u/13741053?v=3&s=460',
-      github_profileUrl: 'https://github.com/kweng2',
+      github_profile_url: 'https://github.com/kweng2',
       elo_rating:1000,
       email: null
     }).save();
@@ -82,7 +82,7 @@ userController.resetWithData = function() {
       github_handle: 'hahnbi',
       github_display_name: 'Hahnbi Sun',
       github_avatar_url: 'https://avatars3.githubusercontent.com/u/12260923?v=3&s=460',
-      github_profileUrl: 'https://github.com/hahnbi',
+      github_profile_url: 'https://github.com/hahnbi',
       elo_rating:1000,
       email: null
     }).save();
